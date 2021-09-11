136a642cb5f7e5cfd075010f2324dc55f3f125a0
Andrei <registr740@yandex.ru>
2021-09-11 17:33:28 +0500

    Initial Commit

---
+++ Finex (v.1.0)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Finex (v.1.0)
+//
+//  Created by Андрей Костицин on 11.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
